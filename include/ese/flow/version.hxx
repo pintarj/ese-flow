ac064abae70cf66169f660e0084d2cae1e47066f
Janez Pintar <pintarj.zero@gmail.com>
2017-09-19 19:51:18 +0200

    added comment to function ese::flow::get_version()

--- include/ese/flow/version.hxx
+++ include/ese/flow/version.hxx
@@ -10,6 +10,12 @@ namespace ese
 {
     namespace flow
     {
+        /**
+         * \brief Get the library version.
+         * \param major An address where will be stored the major version number.
+         * \param minor An address where will be stored the minor version number.
+         * \param build An address where will be stored the build version number.
+         * */
         void get_version(int* major, int* minor = nullptr, int* build = nullptr) noexcept;
     }
 }
